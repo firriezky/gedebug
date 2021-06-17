1e775c5db40c17a1ccf5b0c69dc4c11abe9777aa
Henry Augusta <firriezky@gmail.com>
2021-06-18 00:10:10 +0700

    rollback (add file type at upload)

--- application/controllers/api/Buku.php
+++ application/controllers/api/Buku.php
@@ -64,32 +64,28 @@ class Buku extends REST_Controller
 
     public function index_post()
     {
+        $configPoster = array(
+            'upload_path' => 'uploads/',            //path for upload
+            'allowed_types' => "gif|jpg|png|jpeg",   //restrict extension
+            'file_name' => 'logo_' . date('ymdhis')
+        );
+
+
+        $this->load->library('upload', $configPoster);
 
         $pathPoster = "";
         $pathBuku = "";
-
-        
-        $configPoster = array(
-            'upload_path' => 'uploads/',            //path for upload
-            'file_name' => 'logo_' . date('ymdhis')
-        );
 
         if ($this->upload->do_upload('poster')) {
             $data = array('upload_data' => $this->upload->data());
             $pathPoster = $configPoster['upload_path'] . '' . $data['upload_data']['orig_name'];
         }
 
-        $this->load->library('upload', $configPoster);
-
-    
-
-
         $configBuku = array(
             'upload_path' => 'uploads/',            //path for upload
             'allowed_types' => "pdf",   //restrict extension
             'file_name' => 'book_' . date('ymdhis')
         );
-        
         $this->upload->initialize($configBuku);
 
         if ($this->upload->do_upload('buku')) {
@@ -143,6 +139,7 @@ class Buku extends REST_Controller
 
                 $configPoster = array(
                     'upload_path' => 'uploads/',
+                    'allowed_types' => "gif|jpg|png|jpeg",
                     'file_name' => 'logo_' . date('ymdhis')
                 );
 
@@ -232,6 +229,10 @@ class Buku extends REST_Controller
 
                 $configPoster = array(
                     'upload_path' => 'uploads/',            //path for upload
+                    'allowed_types' => "gif|jpg|png|jpeg",   //restrict extension
+                    'max_size' => '100',
+                    'max_width' => '1024',
+                    'max_height' => '768',
                     'file_name' => 'logo_' . date('ymdhis')
                 );
 
