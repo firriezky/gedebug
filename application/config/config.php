577c6beea4ddf2585b3c4b7f595d305d8b796a28
Henry Augusta <firriezky@gmail.com>
2021-06-18 16:39:21 +0700

    bikin login

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://for-macbook.feylabs.my.id/gedebug/';
+// $config['base_url'] = 'http://for-macbook.feylabs.my.id/gedebug/';
+$config['base_url'] = 'http://localhost/wpU-RestAPI/';
 
 /*
 |--------------------------------------------------------------------------
@@ -314,7 +315,7 @@ $config['cache_query_string'] = FALSE;
 | https://codeigniter.com/user_guide/libraries/encryption.html
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = 'for-macbook';
 
 /*
 |--------------------------------------------------------------------------
--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -1,7 +1,8 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class Welcome extends CI_Controller
+{
 
 	/**
 	 * Index Page for this controller.
@@ -20,6 +21,42 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		$data = array(
+			'title' => 'My Title',
+			'heading' => 'My Heading',
+			'message' => 'My Message'
+		);
+
+		$this->load->view('welcome_message', $data);
+	}
+
+	public function aksi_login()
+	{
+		$username = $this->input->post('username');
+		$password = $this->input->post('password');
+		$where = array(
+			'username' => $username,
+			'password' => md5($password)
+		);
+		$cek = $this->m_login->cek_login("admin", $where)->num_rows();
+		if ($cek > 0) {
+
+			$data_session = array(
+				'nama' => $username,
+				'status' => "login"
+			);
+
+			$this->session->set_userdata($data_session);
+
+			redirect(base_url("admin"));
+		} else {
+			echo "Username dan password salah !";
+		}
+	}
+
+	function logout()
+	{
+		$this->session->sess_destroy();
+		redirect(base_url('login'));
 	}
 }
